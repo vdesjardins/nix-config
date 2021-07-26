b7279c2d89e60379451099fd6e8b3604d4148f34
Vincent Desjardins <vdesjardins@gmail.com>
2021-07-26 14:01:00 +0000

    fix(awscli): completion

--- home/program/awscli/default.nix
+++ home/program/awscli/default.nix
@@ -3,6 +3,6 @@
   home.packages = with pkgs; [ aws-iam-authenticator awscli2 ];
 
   programs.zsh.initExtra = ''
-    complete -C $profile/bin/aws_completer aws
+    complete -C ${pkgs.awscli2}/bin/aws_completer aws
   '';
 }
