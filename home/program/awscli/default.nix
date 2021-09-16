ef5a66bd165da5fab693a1bee6e057202f168be3
Vincent Desjardins <vdesjardins@gmail.com>
2021-09-16 02:06:16 +0000

    feat(aws): profile switcher

--- home/program/awscli/default.nix
+++ home/program/awscli/default.nix
@@ -1,8 +1,19 @@
-{ config, pkgs, ... }:
-{
-  home.packages = with pkgs; [ aws-iam-authenticator awscli2 ];
+{ config, pkgs, lib, ... }:
+with lib;
+mkMerge [
+  {
+    home.packages = with pkgs; [ aws-iam-authenticator awscli2 ];
+  }
+  (
+    mkIf config.programs.zsh.enable {
+      programs.zsh = {
+        initExtra = ''
+          complete -C ${pkgs.awscli2}/bin/aws_completer aws
+        '';
+      };
 
-  programs.zsh.initExtra = ''
-    complete -C ${pkgs.awscli2}/bin/aws_completer aws
-  '';
-}
+      xdg.configFile."zsh/functions/aws_profile".source =
+        ./zsh/functions/aws_profile;
+    }
+  )
+]
