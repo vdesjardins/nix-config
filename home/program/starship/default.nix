33113355323609baad3e90bbd1a93ee494a71afe
Vincent Desjardins <vdesjardins@gmail.com>
2021-09-16 13:03:06 +0000

    feat(starship): make k8s context shorter

--- home/program/starship/default.nix
+++ home/program/starship/default.nix
@@ -1,5 +1,8 @@
-{ ... }: {
-  programs.starship.enable = true;
+{ pkgs, ... }: {
+  programs.starship = {
+    enable = true;
+    package = pkgs.unstable.starship;
+  };
 
   xdg.configFile."starship.toml".source = ./starship.toml;
 }
