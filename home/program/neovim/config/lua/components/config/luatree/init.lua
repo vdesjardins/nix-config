c6dbabfc476b6dbeb3987a2f4de4a13e59beecba
Vincent Desjardins <vdesjardins@gmail.com>
2021-10-05 02:20:55 +0000

    feat(neovim): add dap and mappings + rust-tools config

--- home/program/neovim/config/lua/components/config/luatree/init.lua
+++ home/program/neovim/config/lua/components/config/luatree/init.lua
@@ -1,1 +1,21 @@
-vim.g.nvim_tree_lsp_diagnostics = 1
+require("nvim-tree").setup {
+  disable_netrw = true,
+  hijack_netrw = true,
+  open_on_setup = false,
+  ignore_ft_on_setup = {},
+  update_to_buf_dir = { enable = true, auto_open = true },
+  auto_close = false,
+  open_on_tab = false,
+  hijack_cursor = false,
+  update_cwd = false,
+  lsp_diagnostics = false,
+  update_focused_file = { enable = false, update_cwd = false, ignore_list = {} },
+  system_open = { cmd = nil, args = {} },
+  view = {
+    width = 30,
+    height = 30,
+    side = "left",
+    auto_resize = false,
+    mappings = { custom_only = false, list = {} },
+  },
+}
--- home/program/neovim/config/lua/components/lang/lua/init.lua
+++ home/program/neovim/config/lua/components/lang/lua/init.lua
@@ -1,35 +1,9 @@
 local cfg = {}
 
 function cfg.lsp_setup()
-  local setup = require("components.lang.lua.lua-lsp")
-  setup.cmd = { "lua-language-server" }
-  return setup
-  -- return {
-  --   cmd = { "lua-language-server" },
-  --   on_attach = require"components.config.lsp".common_on_attach,
-  --   settings = {
-  --     Lua = {
-  --       runtime = {
-  --         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-  --         version = "LuaJIT",
-  --         -- Setup your lua path
-  --         path = vim.split(package.path, ";"),
-  --       },
-  --       diagnostics = {
-  --         -- Get the language server to recognize the `vim` global
-  --         globals = { "vim" },
-  --       },
-  --       workspace = {
-  --         -- Make the server aware of Neovim runtime files
-  --         library = {
-  --           [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-  --           [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-  --         },
-  --         maxPreload = 10000,
-  --       },
-  --     },
-  --   },
-  -- }
+  return require("lua-dev").setup({
+    lspconfig = { cmd = { "lua-language-server" } },
+  })
 end
 
 function cfg.lsp_name() return "sumneko_lua" end
