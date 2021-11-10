0f0acdef77633503263fddcc4bc52d95a4bd0cde
Vincent Desjardins <vdesjardins@gmail.com>
2021-11-09 22:01:43 -0500

    feat(darwin): add rectangle for windows management

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -3,6 +3,6 @@
     enable = true;
     brewPrefix =
       if pkgs.system == "aarch64-darwin" then "/opt/homebrew/bin" else "/usr/local/bin";
-    casks = [ "karabiner-elements" "raycast" "google-chrome" ];
+    casks = [ "karabiner-elements" "raycast" "google-chrome" "Rectangle" ];
   };
 }
