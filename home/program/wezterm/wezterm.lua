4b039e4d297a656d6ec813a73dcb3372b4fad76b
Vincent Desjardins <vdesjardins@gmail.com>
2021-11-17 22:11:47 -0500

    feat(wezterm): change theme to github dark

--- home/program/wezterm/wezterm.lua
+++ home/program/wezterm/wezterm.lua
@@ -1,3 +1,3 @@
 local wezterm = require "wezterm";
 return
-  { font = wezterm.font("FiraCode Nerd Font"), color_scheme = "3024 Night" }
+  { font = wezterm.font("FiraCode Nerd Font"), color_scheme = "GitHub Dark" }
