69576f3b2d1d7b4b4c9c512f9d4c0726859b4d35
Vincent Desjardins <vdesjardins@gmail.com>
2021-12-01 20:28:10 -0500

    chore(wezterm): remove added dep updated upstream

--- overlays/wezterm.nix
+++ overlays/wezterm.nix
@@ -1,6 +1,5 @@
 _inputs: _self: super: {
   wezterm = super.wezterm.overrideAttrs (old: {
-    nativeBuildInputs = old.nativeBuildInputs ++ [ super.pkgs.perl ];
     buildInputs = old.buildInputs ++
       super.lib.optionals super.stdenv.isDarwin (with super.pkgs.darwin.apple_sdk.frameworks; [
         UserNotifications
