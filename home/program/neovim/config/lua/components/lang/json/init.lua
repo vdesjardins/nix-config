42fca1a8e5a0b1e29f1170af14c692922520026a
Vincent Desjardins <vdesjardins@gmail.com>
2022-01-01 13:29:23 -0500

    feat(nvim): setup json lsp

--- home/program/neovim/config/lua/components/lang/json/init.lua
+++ home/program/neovim/config/lua/components/lang/json/init.lua
@@ -1,9 +1,13 @@
 local cfg = {}
 
+-- Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 function cfg.lsp_setup()
   return {
     cmd = { "json-languageserver", "--stdio" },
-    on_attach = require"components.config.lsp".common_on_attach,
+    capabilities = capabilities,
   }
 end
 
