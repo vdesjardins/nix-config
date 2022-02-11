abb1e16b983aab92b797d40fa28ed53eea05fe38
Vincent Desjardins <vdesjardins@gmail.com>
2022-02-10 22:10:12 -0500

    feat(rust): use fenix rust-analyzer

--- home/role/dev/rust/default.nix
+++ home/role/dev/rust/default.nix
@@ -1,3 +1,3 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ crate2nix unstable.rust-analyzer unstable.rustup ];
+  home.packages = with pkgs; [ crate2nix fenix.rust-analyzer unstable.rustup ];
 }
