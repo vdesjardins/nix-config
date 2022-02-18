d0a433b88e3750f4ec26f3e5648f14024eb6c237
Vincent Desjardins <vdesjardins@gmail.com>
2022-02-18 18:27:10 +0000

    build: update flake inputs

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
 
     # System
     darwin.url = "github:LnL7/nix-darwin";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-21.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Others
