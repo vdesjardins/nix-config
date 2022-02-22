f7759e9773e819d47052e0d627ab15e8dabae30e
Vincent Desjardins <vdesjardins@gmail.com>
2022-02-22 01:45:06 +0000

    refactor(lua): use unstable sumneko

--- home/role/dev/lua/default.nix
+++ home/role/dev/lua/default.nix
@@ -1,10 +1,9 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    # sumneko-lua-language-server
     lua5_3
     luaformatter
     lua53Packages.luacheck
   ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux (with pkgs;
-    [ sumneko-lua-language-server ]);
+    [ unstable.sumneko-lua-language-server ]);
 }
