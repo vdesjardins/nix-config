682c3d6b8f07f745db2fb0f1dc68a8afc9d7788a
Vincent Desjardins <vdesjardins@gmail.com>
2022-03-10 20:30:21 -0500

    feat(gcloud): add func and alias

--- home/program/gcloud/default.nix
+++ home/program/gcloud/default.nix
@@ -1,6 +1,23 @@
 { pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    google-cloud-sdk
-  ];
-}
+with lib;
+
+mkMerge [
+  (
+    mkIf config.programs.zsh.enable {
+      home.packages = with pkgs; [
+        google-cloud-sdk
+      ];
+
+      programs.zsh = {
+        shellAliases = {
+          gpl = "gcloud projects list";
+        };
+      };
+      xdg.configFile."zsh/functions/gcp-project-id".source =
+        ./zsh/functions/gcp-project-id;
+      xdg.configFile."zsh/functions/gcp-list-ip".source =
+        ./zsh/functions/gcp-list-ip;
+    }
+  )
+]
+
