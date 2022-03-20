9df63e28e2855ad4dbae8e92b759b621cb4666b4
Vincent Desjardins <vdesjardins@gmail.com>
2022-03-20 13:32:59 -0400

    feat(starship): overlay to get latest

--- overlays/unstable/starship.nix
+++ overlays/unstable/starship.nix
@@ -1,51 +1,59 @@
-_inputs: _self: super:
+inputs: _self: super:
 let
+  rustPlatform = super.makeRustPlatform {
+    inherit (inputs.fenix.packages."${super.system}".minimal) cargo rustc;
+  };
+
   inherit (super) lib nixosTests stdenv installShellFiles fetchFromGitHub pkgs pkg-config;
   inherit (pkgs) openssl libiconv;
-  inherit (pkgs.darwin.apple_sdk.frameworks) Security;
+  inherit (pkgs.darwin.apple_sdk.frameworks) Security Foundation;
 in
 {
-  starship = super.rustPlatform.buildRustPackage
-    rec {
-      pname = "starship";
-      version = "1.2.1";
+  starship = rustPlatform.buildRustPackage rec {
+    pname = "starship";
+    version = "1.4.2";
 
-      src = fetchFromGitHub {
-        owner = "starship";
-        repo = pname;
-        rev = "v${version}";
-        sha256 = "sha256-5MJA8eHo1enOHlLpAOF1iDvOHCS/Nw0sc84VWu9nApE=";
-      };
-
-      nativeBuildInputs = [ installShellFiles ] ++ lib.optionals stdenv.isLinux [ pkg-config ];
-
-      buildInputs = lib.optionals stdenv.isLinux [ openssl ]
-        ++ lib.optionals stdenv.isDarwin [ libiconv Security ];
-
-      buildFeatures = lib.optional (!stdenv.isDarwin) "notify-rust";
-
-      postInstall = ''
-        for shell in bash fish zsh; do
-          STARSHIP_CACHE=$TMPDIR $out/bin/starship completions $shell > starship.$shell
-          installShellCompletion starship.$shell
-        done
-      '';
-
-      cargoSha256 = "sha256-DTQQFxj6stzlVzSdmv4J4Nsf8X/VMlwvfIumnuK0YDo=";
-
-      preCheck = ''
-        HOME=$TMPDIR
-      '';
-
-      passthru.tests = {
-        inherit (nixosTests) starship;
-      };
-
-      meta = with lib; {
-        description = "A minimal, blazing fast, and extremely customizable prompt for any shell";
-        homepage = "https://starship.rs";
-        license = licenses.isc;
-        maintainers = with maintainers; [ bbigras danth davidtwco Br1ght0ne Frostman marsam ];
-      };
+    src = fetchFromGitHub {
+      owner = "starship";
+      repo = pname;
+      rev = "v${version}";
+      sha256 = "sha256-eCttQQ6pL8qkA1+O5p0ufsQo5vcypOEYxq+fNhyrdCo=";
     };
+
+    nativeBuildInputs = [ installShellFiles ] ++ lib.optionals stdenv.isLinux [ pkg-config ];
+
+    buildInputs = lib.optionals stdenv.isLinux [ openssl ]
+      ++ lib.optionals stdenv.isDarwin [ libiconv Security Foundation ];
+
+    buildFeatures = lib.optional (!stdenv.isDarwin) "notify-rust";
+
+    postInstall = ''
+      for shell in bash fish zsh; do
+        STARSHIP_CACHE=$TMPDIR $out/bin/starship completions $shell > starship.$shell
+        installShellCompletion starship.$shell
+      done
+    '';
+
+    cargoSha256 = "sha256-QO1zch9b74lFmlgIbktTrxgtRh5lZ6+c9niy81SZK0Q=";
+
+    preCheck = ''
+      HOME=$TMPDIR
+    '';
+
+    passthru.tests = {
+      inherit (nixosTests) starship;
+    };
+
+    NIX_CFLAGS_COMPILE = [
+      # disable modules, otherwise we get redeclaration errors
+      "-fno-modules"
+    ];
+
+    meta = with lib; {
+      description = "A minimal, blazing fast, and extremely customizable prompt for any shell";
+      homepage = "https://starship.rs";
+      license = licenses.isc;
+      maintainers = with maintainers; [ bbigras danth davidtwco Br1ght0ne Frostman marsam ];
+    };
+  };
 }
