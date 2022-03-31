904947d807b143aa9f84134558980af3bce3d8e9
Vincent Desjardins <vdesjardins@gmail.com>
2022-03-31 20:49:59 +0000

    feat(openssl): add tls decode bundle func

--- home/program/openssl/default.nix
+++ home/program/openssl/default.nix
@@ -7,4 +7,6 @@ with lib;
 
   xdg.configFile."zsh/functions/tls-server-dump".source =
     mkIf config.programs.zsh.enable ./zsh/functions/tls-server-dump;
+  xdg.configFile."zsh/functions/tls-decode-bundle".source =
+    mkIf config.programs.zsh.enable ./zsh/functions/tls-decode-bundle;
 }
