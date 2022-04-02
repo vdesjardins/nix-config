abe3288e702630881338268443dc22965f58f606
Vincent Desjardins <vdesjardins@gmail.com>
2022-04-02 16:52:37 -0400

    feat(debugging): add uftrace

--- home/role/dev/debugging/default.nix
+++ home/role/dev/debugging/default.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ xxd gdb cgdb gdbgui binutils lldb ] ++ lib.optionals stdenv.isLinux [
+  home.packages = with pkgs; [
+    binutils
+    cgdb
+    gdb
+    gdbgui
+    lldb
+    xxd
+  ] ++ lib.optionals stdenv.isLinux [
+    uftrace # Function graph tracer for C/C++/Rust
     rr-unstable
   ];
 }
