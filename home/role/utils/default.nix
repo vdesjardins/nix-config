fdbbf6d4772ab4635255da1de08845c93ccf78a1
Vincent Desjardins <vdesjardins@gmail.com>
2022-04-03 11:15:56 -0400

    chore(utils): sort pkgs

--- home/role/utils/default.nix
+++ home/role/utils/default.nix
@@ -21,44 +21,44 @@
 
   home.packages = with pkgs;
     [
+      # unstable.fq # jq for binary formats
       act # github actions testing
       bandwhich # network utilization by process
-      coreutils
       bottom # top with graphs
       browsh # terminal browser
       cachix
       cloc # source code line counter
       cmake
+      coreutils
       ctags
       curl
       du-dust # fancy du
       duf # df alternative
       fd # fast file search
       findutils
-      # unstable.fq # jq for binary formats
       glow # renders markdown on command line
       gnumake
       gnused
       graphviz
       grpcurl
+      hexyl
       htmlq # Like jq, but for HTML
       htop # fancy top
-      hexyl
       hyperfine
       iotop
       jq
       lazygit
       lsof
       oha # HTTP load generator inspired by rakyll/hey with tui animation
+      openssh
+      perlPackages.vidir # edit files in EDITOR
       ps
       ripgrep
       rsync
       spotify-tui
-      openssh
       tealdeer # faster tldr
       tokei # displays code statistics
       tree
-      perlPackages.vidir # edit files in EDITOR
     ] ++ lib.optionals stdenv.isLinux [
       unstable.btop # monitor resources
       pueue # task management tool
