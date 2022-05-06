8d14780c6a3d57b18e883959d93924f68caf1756
Vincent Desjardins <vdesjardins@gmail.com>
2022-05-05 22:17:26 -0400

    fix(wezterm): missing default values on linux

--- home/programs/wezterm/default.nix
+++ home/programs/wezterm/default.nix
@@ -4,18 +4,22 @@ let
   wezterm_config = builtins.replaceStrings [ "@homeDirectory@" ] [ config.home.homeDirectory ] wezterm_text;
   pkg_wezterm = pkgs.master.wezterm;
 in
-{
-  home.packages = [ pkg_wezterm ];
-  xdg.configFile."wezterm/wezterm.lua".text = wezterm_config;
+lib.mkMerge [
+  {
+    home.packages = [ pkg_wezterm ];
+    xdg.configFile."wezterm/wezterm.lua".text = wezterm_config;
 
-  programs.zsh = {
-    initExtra = ''
-      source ${pkg_wezterm}/etc/profile.d/wezterm.sh
-    '';
-  };
-} // lib.mkIf pkgs.stdenv.isDarwin {
-  # home-manager do not yet symlink to ~/Applications
-  # https://github.com/nix-community/home-manager/issues/1341
-  home.file."Applications/WezTerm.app".source =
-    "${pkg_wezterm}/Applications/WezTerm.app";
-}
+    programs.zsh = {
+      initExtra = ''
+        source ${pkg_wezterm}/etc/profile.d/wezterm.sh
+      '';
+    };
+  }
+  (lib.mkIf pkgs.stdenv.isDarwin
+    {
+      # home-manager do not yet symlink to ~/Applications
+      # https://github.com/nix-community/home-manager/issues/1341
+      home.file."Applications/WezTerm.app".source =
+        "${pkg_wezterm}/Applications/WezTerm.app";
+    })
+]
