3d0a9e7c3da9938515d01c20d2e4755cf9d3b318
Vincent Desjardins <vdesjardins@gmail.com>
2022-05-05 22:16:51 -0400

    fix(alacritty): missing default values on linux

--- home/programs/alacritty/default.nix
+++ home/programs/alacritty/default.nix
@@ -1,11 +1,14 @@
-{ lib, pkgs, ... }: {
-  programs.alacritty = { enable = true; };
+{ lib, pkgs, ... }: lib.mkMerge [
+  {
+    programs.alacritty = { enable = true; };
 
-  xdg.configFile."alacritty/alacritty.yml".source = ./alacritty.yml;
-
-} // lib.mkIf pkgs.stdenv.isDarwin {
-  # home-manager do not yet symlink to ~/Applications
-  # https://github.com/nix-community/home-manager/issues/1341
-  home.file."Applications/Alacritty.app".source =
-    "${pkgs.alacritty}/Applications/Alacritty.app";
-}
+    xdg.configFile."alacritty/alacritty.yml".source = ./alacritty.yml;
+  }
+  (lib.mkIf pkgs.stdenv.isDarwin
+    {
+      # home-manager do not yet symlink to ~/Applications
+      # https://github.com/nix-community/home-manager/issues/1341
+      home.file."Applications/Alacritty.app".source =
+        "${pkgs.alacritty}/Applications/Alacritty.app";
+    })
+]
