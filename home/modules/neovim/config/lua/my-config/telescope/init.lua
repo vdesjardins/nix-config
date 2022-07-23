cb0b517b655ff95cf14b50b04836632166e1d805
Vincent Desjardins <vdesjardins@gmail.com>
2022-07-23 19:58:35 -0400

    chore(neovim): apply lua formatting

--- home/modules/neovim/config/lua/my-config/telescope/init.lua
+++ home/modules/neovim/config/lua/my-config/telescope/init.lua
@@ -4,33 +4,33 @@ local trouble = require("trouble.providers.telescope")
 local telescope = require("telescope")
 
 telescope.setup({
-  defaults = {
-    mappings = {
-      i = {
-        ["<c-t>"] = trouble.open_with_trouble,
-        ["<C-h>"] = "which_key",
-      },
-      n = { ["<c-t>"] = trouble.open_with_trouble },
+    defaults = {
+        mappings = {
+            i = {
+                ["<c-t>"] = trouble.open_with_trouble,
+                ["<C-h>"] = "which_key",
+            },
+            n = { ["<c-t>"] = trouble.open_with_trouble },
+        },
     },
-  },
-  extensions = {
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown({}),
+    extensions = {
+        ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+        },
     },
-  },
 })
 
 telescope.load_extension("ui-select")
 
 local wk = require("which-key")
 wk.register({
-  p = {
-    name = "projects",
-    f = { "<cmd>Telescope find_files<cr>", "find-file-in-project" },
-    s = { "<cmd>Telescope live_grep<cr>", "search-in-project" },
-    m = { "<cmd>Telescope marks<cr>", "bookmarks" },
-    h = { "<cmd>Telescope help_tags<cr>", "help" },
-  },
+    p = {
+        name = "projects",
+        f = { "<cmd>Telescope find_files<cr>", "find-file-in-project" },
+        s = { "<cmd>Telescope live_grep<cr>", "search-in-project" },
+        m = { "<cmd>Telescope marks<cr>", "bookmarks" },
+        h = { "<cmd>Telescope help_tags<cr>", "help" },
+    },
 }, { prefix = "<leader>" })
 
 vim.api.nvim_set_keymap("n", "gs", "<plug>(GrepperOperator)", { silent = true })
