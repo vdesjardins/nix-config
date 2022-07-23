35779dad9eacc83ed0dc188cf9eb9c161cc36a73
Vincent Desjardins <vdesjardins@gmail.com>
2022-07-23 19:55:53 -0400

    feat(lua): use plugin to setup nvim lua

--- home/modules/neovim/config/lua/my-lang/lua/init.lua
+++ home/modules/neovim/config/lua/my-lang/lua/init.lua
@@ -1,4 +1,14 @@
-require("lspconfig").sumneko_lua.setup(require("my-lang.lua.lua-lsp"))
+local luadev = require("lua-dev").setup({
+    lspconfig = {
+        settings = {
+            Lua = {
+                formatter = { enable = false },
+            },
+        },
+    },
+})
+
+require("lspconfig").sumneko_lua.setup(luadev)
 
 local null_ls = require("null-ls")
 _G.null_ls_sources[#_G.null_ls_sources + 1] = null_ls.builtins.formatting.stylua
