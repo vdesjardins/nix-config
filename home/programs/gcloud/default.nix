97d36ef27e4d5df809fd3dc2a467665651686a08
Vincent Desjardins <vincent.desjardins@wunderkind.co>
2022-10-24 09:15:48 -0400

    feat(gcloud): add auth plugin

--- home/programs/gcloud/default.nix
+++ home/programs/gcloud/default.nix
@@ -4,9 +4,13 @@ with lib;
 mkMerge [
   (
     mkIf config.programs.zsh.enable {
-      home.packages = with pkgs; [
-        google-cloud-sdk
+      home.packages = with pkgs.unstable; [
+        (google-cloud-sdk.withExtraComponents [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
       ];
+
+      home.sessionVariables = {
+        USE_GKE_GCLOUD_AUTH_PLUGIN = "True";
+      };
 
       programs.zsh = {
         shellAliases = {
