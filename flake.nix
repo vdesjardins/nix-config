dcaf6d1a9e8600f5ea5025ae082291fe6dab9139
Vincent Desjardins <vdesjardins@gmail.com>
2022-11-09 09:00:33 -0500

    fix: mac build

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
 
     # System
     darwin.url = "github:LnL7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-22.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
--- modules/shared.nix
+++ modules/shared.nix
@@ -1,14 +1,16 @@
 # This file contains configuration that is shared across all hosts.
 { pkgs, ... }: {
   nix = {
-    binaryCaches = [
-      "https://cache.nixos.org/"
-      "https://vdesjardins.cachix.org"
-    ];
-    binaryCachePublicKeys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "vdesjardins.cachix.org-1:o0VX1pROMi3RAULObu4+OOCIZFpcKAR01Oxef2CdUx4="
-    ];
+    settings = {
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://vdesjardins.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "vdesjardins.cachix.org-1:o0VX1pROMi3RAULObu4+OOCIZFpcKAR01Oxef2CdUx4="
+      ];
+    };
 
     gc = {
       automatic = true;
