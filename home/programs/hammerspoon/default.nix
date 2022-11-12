92cd614c3e3fadd76631575e11cdad553af2f863
Vincent Desjardins <vdesjardins@gmail.com>
2022-11-12 15:35:52 -0500

    fix(hammerspoon): init.lua link

--- home/programs/hammerspoon/default.nix
+++ home/programs/hammerspoon/default.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, ... }:
 {
   home.file.".hammerspoon/Spoons/ControlEscape.spoon/".source = "${pkgs.hammerspoon-controlescape}/share/hammerspoon/controlescape/";
-  xdg.configFile.".hammerspoon/init.lua".source = ./init.lua;
+  home.file.".hammerspoon/init.lua".source = ./init.lua;
 }
