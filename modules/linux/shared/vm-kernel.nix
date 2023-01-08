338e330a6299ef4a2d72b058e58d90d476fad77c
Vincent Desjardins <vdesjardins@gmail.com>
2023-01-08 13:06:15 -0500

    chore(vm-kernel): remove useless comment

--- modules/linux/shared/vm-kernel.nix
+++ modules/linux/shared/vm-kernel.nix
@@ -1,7 +1,4 @@
 { config, pkgs, ... }:
 {
-  # We require 5.14+ for VMware Fusion on M1.
-  # Can't use latest to build because kernel security fixes make it unbootable
-  # https://github.com/mitchellh/nixos-config/issues/22
   boot.kernelPackages = pkgs.linuxPackages_latest;
 }
