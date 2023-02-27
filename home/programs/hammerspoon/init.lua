8a70e0a79e9fdc0c0807d8370ec28a94f7e79d76
Vincent Desjardins <vdesjardins@gmail.com>
2023-02-26 19:19:04 -0500

    feat(hammperspoon): manage section char on non us kb

--- home/programs/hammerspoon/init.lua
+++ home/programs/hammerspoon/init.lua
@@ -1,1 +1,11 @@
 hs.loadSpoon("ControlEscape"):start()
+-- replace section character with backtick on non us keyboards
+tildeHotkey = hs.hotkey.new("shift", "§", function()
+    hs.eventtap.keyStroke("shift", "`")
+end)
+tildeHotkey:enable()
+
+graveAccentHotkey = hs.hotkey.new(nil, "§", function()
+    hs.eventtap.keyStroke(nil, "`")
+end)
+graveAccentHotkey:enable()
