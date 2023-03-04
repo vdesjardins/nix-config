30ceea1b6dba32422f38f11685c82d8ed623260e
Vincent Desjardins <vdesjardins@gmail.com>
2023-03-04 13:44:41 -0500

    fix(snippets): double loading

--- home/modules/neovim/config/lua/my-config/ultisnips/init.lua
+++ home/modules/neovim/config/lua/my-config/ultisnips/init.lua
@@ -1,14 +1,14 @@
 vim.g.UltiSnipsExpandTrigger = "<TAB>"
 vim.g.UltiSnipsJumpForwardTrigger = "<TAB>"
 vim.g.UltiSnipsJumpBackwardTrigger = "<S-TAB>"
-vim.g.UltiSnipsSnippetDirectories = { "UltiSnips", "ultisnips" }
+vim.g.UltiSnipsSnippetDirectories = { "UltiSnips", "lang-snippets" }
 
 -- add all language snippets to runtimepath
 local Path = require("plenary.path")
 local current_file_path = debug.getinfo(1).source:match("@?(.*/)")
 local my_lang_path = current_file_path .. "../../my-lang/"
 for _, p in
-    ipairs(vim.fs.find("ultisnips", { path = my_lang_path, type = "directory", limit = 50 }))
+    ipairs(vim.fs.find("lang-snippets", { path = my_lang_path, type = "directory", limit = 50 }))
 do
     vim.opt.runtimepath:append(Path:new(p .. "/.."):absolute())
 end
