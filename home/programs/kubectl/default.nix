313917b455cad909a6fc6ae8231c6090713a474a
Vincent Desjardins <vdesjardins@gmail.com>
2023-03-17 15:15:19 -0400

    feat(kubectl): add eks update config for one cluster

--- home/programs/kubectl/default.nix
+++ home/programs/kubectl/default.nix
@@ -102,6 +102,8 @@ with lib;
           ./zsh/functions/kube-get-pod-images;
         xdg.configFile."zsh/functions/kube-updateconfig-aws".source =
           ./zsh/functions/kube-updateconfig-aws;
+        xdg.configFile."zsh/functions/kube-updateconfig-aws-all".source =
+          ./zsh/functions/kube-updateconfig-aws-all;
         xdg.configFile."zsh/functions/kube-get-node-pods".source =
           ./zsh/functions/kube-get-node-pods;
         xdg.configFile."zsh/functions/kube-node-connect".source =
