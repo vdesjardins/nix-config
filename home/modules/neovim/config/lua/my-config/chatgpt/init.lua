628fb0c2029ff6122c3bef94a19e10cd26dfff20
Vincent Desjardins <vdesjardins@gmail.com>
2023-03-19 10:15:13 -0400

    feat(neovim): disable chatgpt if api key not set

--- home/modules/neovim/config/lua/my-config/chatgpt/init.lua
+++ home/modules/neovim/config/lua/my-config/chatgpt/init.lua
@@ -1,17 +1,21 @@
-require("chatgpt").setup({
-    keymaps = {
-        close = { "<C-c>" },
-        submit = "<C-e>",
-        yank_last = "<C-y>",
-        yank_last_code = "<C-k>",
-        scroll_up = "<C-u>",
-        scroll_down = "<C-d>",
-        toggle_settings = "<C-o>",
-        new_session = "<C-n>",
-        cycle_windows = "<Tab>",
-        -- in the Sessions pane
-        select_session = "<Space>",
-        rename_session = "r",
-        delete_session = "d",
-    },
-})
+local os = require("os")
+
+if os.getenv("OPENAI_API_KEY") then
+    require("chatgpt").setup({
+        keymaps = {
+            close = { "<C-c>" },
+            submit = "<C-e>",
+            yank_last = "<C-y>",
+            yank_last_code = "<C-k>",
+            scroll_up = "<C-u>",
+            scroll_down = "<C-d>",
+            toggle_settings = "<C-o>",
+            new_session = "<C-n>",
+            cycle_windows = "<Tab>",
+            -- in the Sessions pane
+            select_session = "<Space>",
+            rename_session = "r",
+            delete_session = "d",
+        },
+    })
+end
