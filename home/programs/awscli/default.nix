882ad39dfd64983070a7c021c2fb7db0f3a117ff
Vincent Desjardins <vdesjardins@gmail.com>
2023-10-18 17:00:22 -0400

    feat(awscli): add profile helpers

--- home/programs/awscli/default.nix
+++ home/programs/awscli/default.nix
@@ -7,7 +7,7 @@
 with lib;
   mkMerge [
     {
-      home.packages = with pkgs; [aws-iam-authenticator awscli2];
+      home.packages = with pkgs; [aws-iam-authenticator awscli2 aws-sso-creds];
       home.file.".aws/cli/alias".source = ./alias;
     }
     (
@@ -16,14 +16,23 @@ with lib;
           initExtra = ''
             complete -C ${pkgs.awscli2}/bin/aws_completer aws
 
-            function aws-find-profiles() {
+            function aws-profile-name-from-id() {
               aws_account_id=$1
               jc --ini <~/.aws/config | jq 'to_entries | .[] | select(.value.sso_account_id == "'"$aws_account_id"'") | .key | split("\\s+"; "")[1]' -Mr
             }
+            function aws-profile-id-from-name() {
+              aws_account_name=$1
+              jc --ini <~/.aws/config | jq '[ to_entries | .[] | select((.key | split("(\\.|\\s+)"; "")[1]) == "'"$aws_account_name"'") | .value.sso_account_id ] | unique | .[]' -Mr
+            }
+
           '';
           envExtra = ''
             export AWS_PAGER=""
           '';
+
+          shellAliases = {
+            aws-creds = "eval $(aws-sso-creds export)";
+          };
         };
 
         xdg.configFile."zsh/functions/aws-profile".source =
