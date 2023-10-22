2f957deeec020bc559005247c12232b113410084
Vincent Desjardins <vdesjardins@gmail.com>
2023-10-21 20:10:29 -0400

    feat(wezterm): switch to nord theme

--- home/programs/wezterm/wezterm-tmux.nix
+++ home/programs/wezterm/wezterm-tmux.nix
@@ -3,44 +3,10 @@
 
   wezterm.add_to_config_reload_watch_list(wezterm.config_dir)
 
-  -- tokyo night - night
-  local function color_defs()
-      return {
-          foreground = "#c0caf5",
-          background = "#1a1b26",
-          cursor_bg = "#c0caf5",
-          cursor_border = "#c0caf5",
-          cursor_fg = "#1a1b26",
-          selection_bg = "#33467C",
-          selection_fg = "#c0caf5",
-
-          ansi = {
-              "#15161E",
-              "#f7768e",
-              "#9ece6a",
-              "#e0af68",
-              "#7aa2f7",
-              "#bb9af7",
-              "#7dcfff",
-              "#a9b1d6",
-          },
-          brights = {
-              "#414868",
-              "#f7768e",
-              "#9ece6a",
-              "#e0af68",
-              "#7aa2f7",
-              "#bb9af7",
-              "#7dcfff",
-              "#c0caf5",
-          },
-      }
-  end
-
   local config = {
       check_for_updates = false,
       font = wezterm.font("JetBrainsMono Nerd Font"),
-      colors = color_defs(),
+      color_scheme = 'nord',
       tab_bar_at_bottom = true,
       inactive_pane_hsb = { hue = 1.0, saturation = 0.5, brightness = 1.0 },
       exit_behavior = "Close",
