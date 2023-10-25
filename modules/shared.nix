a2ec812d13ce88426cc7cc0d88b15c1597cdefe6
Vincent Desjardins <vdesjardins@gmail.com>
2023-10-25 16:48:53 -0400

    feat(nix): add ca-derivations cache

--- modules/shared.nix
+++ modules/shared.nix
@@ -5,10 +5,12 @@
       substituters = [
         "https://cache.nixos.org/"
         "https://vdesjardins.cachix.org"
+        "https://cache.ngi0.nixos.org/" # ca-derivations cache
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "vdesjardins.cachix.org-1:o0VX1pROMi3RAULObu4+OOCIZFpcKAR01Oxef2CdUx4="
+        "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
       ];
     };
 
