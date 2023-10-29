a1b9fd4c73f438a6a5b348d154731bb5af133c95
Vincent Desjardins <vdesjardins@gmail.com>
2023-10-29 10:36:36 -0400

    feat(darwin): app launchers wrapper for spotlight

---
+++ home/modules/darwin/app-symlinks/default.nix
@@ -0,0 +1,34 @@
+# https://github.com/nix-community/home-manager/issues/1341#issuecomment-1716147796
+{
+  config,
+  pkgs,
+  lib,
+  ...
+} @ args: {
+  home.activation = {
+    trampolineApps = let
+      apps = pkgs.buildEnv {
+        name = "home-manager-applications";
+        paths = config.home.packages;
+        pathsToLink = "/Applications";
+      };
+    in
+      lib.hm.dag.entryAfter ["writeBoundary"] ''
+        toDir="$HOME/Applications/Home Manager Trampolines"
+        fromDir="${apps}/Applications"
+        rm -rf "$toDir"
+        mkdir "$toDir"
+        (
+          cd "$fromDir"
+          for app in *.app; do
+            /usr/bin/osacompile -o "$toDir/$app" -e 'do shell script "open \"'$fromDir/$app'\""'
+            icon="$(/usr/bin/plutil -extract CFBundleIconFile raw "$fromDir/$app/Contents/Info.plist")"
+            mkdir -p "$toDir/$app/Contents/Resources"
+            if [[ -f "$fromDir/$app/Contents/Resources/$icon" ]]; then
+              cp -f "$fromDir/$app/Contents/Resources/$icon" "$toDir/$app/Contents/Resources/applet.icns"
+            fi
+          done
+        )
+      '';
+  };
+}
