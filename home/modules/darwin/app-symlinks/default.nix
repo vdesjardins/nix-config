ab2e66d31fd3bd57b6bae6b8e4a260f23c64d573
Vincent Desjardins <vdesjardins@gmail.com>
2023-10-29 11:01:00 -0400

    fix(darwin): only run app launchers activation on darwin

--- home/modules/darwin/app-symlinks/default.nix
+++ home/modules/darwin/app-symlinks/default.nix
@@ -4,31 +4,35 @@
   pkgs,
   lib,
   ...
-} @ args: {
-  home.activation = {
-    trampolineApps = let
-      apps = pkgs.buildEnv {
-        name = "home-manager-applications";
-        paths = config.home.packages;
-        pathsToLink = "/Applications";
+} @ args:
+with lib;
+  mkMerge [
+    (mkIf pkgs.stdenv.hostPlatform.isDarwin {
+      home.activation = {
+        trampolineApps = let
+          apps = pkgs.buildEnv {
+            name = "home-manager-applications";
+            paths = config.home.packages;
+            pathsToLink = "/Applications";
+          };
+        in
+          lib.hm.dag.entryAfter ["writeBoundary"] ''
+            toDir="$HOME/Applications/Home Manager Trampolines"
+            fromDir="${apps}/Applications"
+            rm -rf "$toDir"
+            mkdir "$toDir"
+            (
+              cd "$fromDir"
+              for app in *.app; do
+                /usr/bin/osacompile -o "$toDir/$app" -e 'do shell script "open \"'$fromDir/$app'\""'
+                icon="$(/usr/bin/plutil -extract CFBundleIconFile raw "$fromDir/$app/Contents/Info.plist")"
+                mkdir -p "$toDir/$app/Contents/Resources"
+                if [[ -f "$fromDir/$app/Contents/Resources/$icon" ]]; then
+                  cp -f "$fromDir/$app/Contents/Resources/$icon" "$toDir/$app/Contents/Resources/applet.icns"
+                fi
+              done
+            )
+          '';
       };
-    in
-      lib.hm.dag.entryAfter ["writeBoundary"] ''
-        toDir="$HOME/Applications/Home Manager Trampolines"
-        fromDir="${apps}/Applications"
-        rm -rf "$toDir"
-        mkdir "$toDir"
-        (
-          cd "$fromDir"
-          for app in *.app; do
-            /usr/bin/osacompile -o "$toDir/$app" -e 'do shell script "open \"'$fromDir/$app'\""'
-            icon="$(/usr/bin/plutil -extract CFBundleIconFile raw "$fromDir/$app/Contents/Info.plist")"
-            mkdir -p "$toDir/$app/Contents/Resources"
-            if [[ -f "$fromDir/$app/Contents/Resources/$icon" ]]; then
-              cp -f "$fromDir/$app/Contents/Resources/$icon" "$toDir/$app/Contents/Resources/applet.icns"
-            fi
-          done
-        )
-      '';
-  };
-}
+    })
+  ]
