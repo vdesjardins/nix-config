b7254e876691ac16c11c9c3fd9709cca438a18c6
Vincent Desjardins <vdesjardins@gmail.com>
2023-11-04 10:27:55 -0400

    feat(neovim): harpoon tuning

--- home/modules/neovim/config/lua/my-config/harpoon/init.lua
+++ home/modules/neovim/config/lua/my-config/harpoon/init.lua
@@ -1,7 +1,12 @@
 require("telescope").load_extension("harpoon")
+require("harpoon").setup({
+    menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
+    },
+})
 
 local wk = require("which-key")
-wk.register({
+local mappings = {
     m = {
         name = "harpoon (mark)",
         m = { '<cmd>lua require("harpoon.mark").add_file()<cr>', "add-file" },
@@ -9,4 +14,11 @@ wk.register({
         n = { '<cmd>lua require("harpoon.ui").nav_next()<cr>', "next" },
         p = { '<cmd>lua require("harpoon.ui").nav_prev()<cr>', "previous" },
     },
-}, { prefix = "<leader>" })
+}
+
+for i = 1, 9, 1 do
+    mappings.m[tostring(i)] =
+        { '<cmd>lua require("harpoon.ui").nav_file(' .. i .. ")<cr>", "goto " .. tostring(i) }
+end
+
+wk.register(mappings, { prefix = "<leader>" })
