7b99f0bf8eb5c53961fb5f901cae9279ca166442
Vincent Desjardins <vdesjardins@gmail.com>
2023-11-05 21:09:28 -0500

    feat(neovim): tune go lsp

--- home/modules/neovim/config/lua/my-lang/go/init.lua
+++ home/modules/neovim/config/lua/my-lang/go/init.lua
@@ -1,7 +1,11 @@
-local capabilities =
-    require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 require("lspconfig").gopls.setup({
-    cmd = { "gopls" },
-    on_attach = require("my-config.lsp").common_on_attach,
-    capabilities = capabilities,
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true,
+            },
+            staticcheck = true,
+            gofumpt = true,
+        },
+    },
 })
