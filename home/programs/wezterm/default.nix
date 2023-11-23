3951039ba13c6998d83cda13b399706386096ec5
Vincent Desjardins <vdesjardins@gmail.com>
2023-11-22 21:38:00 -0500

    feat(wezterm): remove mac install workaround

--- home/programs/wezterm/default.nix
+++ home/programs/wezterm/default.nix
@@ -6,22 +6,13 @@
 }: let
   wezterm_config = import ./wezterm-tmux.nix {inherit (config.home) homeDirectory;};
   pkg_wezterm = pkgs.unstable.wezterm;
-in
-  lib.mkMerge [
-    {
-      home.packages = [pkg_wezterm];
-      xdg.configFile."wezterm/wezterm.lua".text = wezterm_config;
+in {
+  home.packages = [pkg_wezterm];
+  xdg.configFile."wezterm/wezterm.lua".text = wezterm_config;
 
-      programs.zsh = {
-        initExtra = ''
-          source ${pkg_wezterm}/etc/profile.d/wezterm.sh
-        '';
-      };
-    }
-    (lib.mkIf pkgs.stdenv.isDarwin
-      {
-        # home-manager do not yet symlink to ~/Applications
-        # https://github.com/nix-community/home-manager/issues/1341
-        home.file."Applications/WezTerm.app".source = "${pkg_wezterm}/Applications/WezTerm.app";
-      })
-  ]
+  programs.zsh = {
+    initExtra = ''
+      source ${pkg_wezterm}/etc/profile.d/wezterm.sh
+    '';
+  };
+}
