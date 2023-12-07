9f0be7ef914ceb7aa4d3d92d5d2ebd7df8313cb2
Vincent Desjardins <vdesjardins@gmail.com>
2023-12-06 19:50:15 -0500

    refactor(ripgrep): use hm module

--- home/programs/ripgrep/default.nix
+++ home/programs/ripgrep/default.nix
@@ -1,26 +1,17 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: let
-  # TODO: refactor after hm 23.11 release
-  configPath = "${config.xdg.configHome}/ripgrep/ripgreprc";
-  arguments = [
-    "--max-columns=150"
-    "--max-columns-preview"
-    "--hidden"
-    "--type-add=zshrc:*.zsh*"
-    "--type-add=bashrc:*.sh*"
-    "--type-add=automake:*makefile.in*"
-    "--glob=!git/*"
-    "--glob=!.git/*"
-    "--ignore-case"
-  ];
-in {
-  home.packages = [pkgs.ripgrep];
+{config, ...}: {
+  programs.ripgrep = {
+    enable = true;
 
-  home.file."${configPath}".text = lib.concatLines arguments;
-
-  home.sessionVariables."RIPGREP_CONFIG_PATH" = configPath;
+    arguments = [
+      "--max-columns=150"
+      "--max-columns-preview"
+      "--hidden"
+      "--type-add=zshrc:*.zsh*"
+      "--type-add=bashrc:*.sh*"
+      "--type-add=automake:*makefile.in*"
+      "--glob=!git/*"
+      "--glob=!.git/*"
+      "--ignore-case"
+    ];
+  };
 }
