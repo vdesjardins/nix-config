ed257e5f0ae66710374bef84e41b6b6594612cd3
Vincent Desjardins <vdesjardins@gmail.com>
2023-12-09 09:48:49 -0500

    refactor(ls): change zsh loading

--- home/programs/ls/default.nix
+++ home/programs/ls/default.nix
@@ -6,8 +6,6 @@
   home.file.".dircolors".source = "${pkgs.lscolors}/share/lscolors/LS_COLORS";
 
   programs.zsh.initExtra = ''
-    source <(dircolors ${
-      config.home.homeDirectory + "/" + config.home.file.".dircolors".target
-    })
+    eval $(${pkgs.coreutils}/bin/dircolors -b ~/.dircolors)
   '';
 }
