39648c844907cd848aa54953bdaec54c74e5f375
Vincent Desjardins <vdesjardins@gmail.com>
2023-12-13 18:52:02 -0500

    feat(darwin): brew cleanup and upgrade

--- modules/darwin/shared/homebrew.nix
+++ modules/darwin/shared/homebrew.nix
@@ -5,5 +5,10 @@
       if pkgs.system == "aarch64-darwin"
       then "/opt/homebrew/bin"
       else "/usr/local/bin";
+    onActivation = {
+      cleanup = "zap";
+      autoUpdate = true;
+      upgrade = true;
+    };
   };
 }
