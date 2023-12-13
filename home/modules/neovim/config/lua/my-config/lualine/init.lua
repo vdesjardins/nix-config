43f1b191b5938a8ee1dc7dc7799161114837433d
Vincent Desjardins <vdesjardins@gmail.com>
2023-12-12 19:43:16 -0500

    feat(neovim): add noice info in status line

--- home/modules/neovim/config/lua/my-config/lualine/init.lua
+++ home/modules/neovim/config/lua/my-config/lualine/init.lua
@@ -52,7 +52,30 @@ require("lualine").setup({
                 color = { fg = "#7195e1", gui = "bold" },
             },
         },
-        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_x = {
+            {
+                require("noice").api.status.message.get_hl,
+                cond = require("noice").api.status.message.has,
+            },
+            {
+                require("noice").api.status.command.get,
+                cond = require("noice").api.status.command.has,
+                color = { fg = "#ff9e64" },
+            },
+            {
+                require("noice").api.status.mode.get,
+                cond = require("noice").api.status.mode.has,
+                color = { fg = "#ff9e64" },
+            },
+            {
+                require("noice").api.status.search.get,
+                cond = require("noice").api.status.search.has,
+                color = { fg = "#ff9e64" },
+            },
+            "encoding",
+            "fileformat",
+            "filetype",
+        },
         lualine_y = { "progress" },
         lualine_z = { "location" },
     },
