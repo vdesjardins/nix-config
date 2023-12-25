3da45a32693ea5720ab6ff8e37da022116fad463
Vincent Desjardins <vdesjardins@gmail.com>
2023-12-25 14:56:47 -0500

    feat(wezterm): configure copy in clipboard

--- home/modules/wezterm/wezterm-tmux.nix
+++ home/modules/wezterm/wezterm-tmux.nix
@@ -20,6 +20,14 @@ lua
     return hyperlink_rules
   end
 
+  function make_mouse_binding(dir, streak, button, mods, action)
+    return {
+      event = { [dir] = { streak = streak, button = button } },
+      mods = mods,
+      action = action,
+    }
+  end
+
   local config = {
       check_for_updates = false,
       font = wezterm.font({
@@ -33,11 +41,17 @@ lua
       exit_behavior = "Close",
 
       mouse_bindings = {
-          {
-              event = { Down = { streak = 3, button = "Left" } },
-              action = { SelectTextAtMouseCursor = "SemanticZone" },
-              mods = "NONE",
-          },
+        {
+            event = { Down = { streak = 3, button = "Left" } },
+            action = { SelectTextAtMouseCursor = "SemanticZone" },
+            mods = "NONE",
+        },
+        make_mouse_binding('Up', 1, 'Left', 'NONE', wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor 'ClipboardAndPrimarySelection'),
+        make_mouse_binding('Up', 1, 'Left', 'SHIFT', wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor 'ClipboardAndPrimarySelection'),
+        make_mouse_binding('Up', 1, 'Left', 'ALT', wezterm.action.CompleteSelection 'ClipboardAndPrimarySelection'),
+        make_mouse_binding('Up', 1, 'Left', 'SHIFT|ALT', wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor 'ClipboardAndPrimarySelection'),
+        make_mouse_binding('Up', 2, 'Left', 'NONE', wezterm.action.CompleteSelection 'ClipboardAndPrimarySelection'),
+        make_mouse_binding('Up', 3, 'Left', 'NONE', wezterm.action.CompleteSelection 'ClipboardAndPrimarySelection'),
       },
 
       pane_focus_follows_mouse = true,
--- home/modules/wezterm/wezterm.nix
+++ home/modules/wezterm/wezterm.nix
@@ -61,6 +61,13 @@ lua
     }
   end
 
+  function make_mouse_binding(dir, streak, button, mods, action)
+    return {
+      event = { [dir] = { streak = streak, button = button } },
+      mods = mods,
+      action = action,
+    }
+  end
 
   local config = {
     check_for_updates = false,
@@ -82,11 +89,17 @@ lua
     leader = { key = "${key_leader}", mods = "${mods_leader}" },
 
     mouse_bindings = {
-        {
-            event = { Down = { streak = 3, button = "Left" } },
-            action = { SelectTextAtMouseCursor = "SemanticZone" },
-            mods = "NONE",
-        },
+      {
+          event = { Down = { streak = 3, button = "Left" } },
+          action = { SelectTextAtMouseCursor = "SemanticZone" },
+          mods = "NONE",
+      },
+      make_mouse_binding('Up', 1, 'Left', 'NONE', wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor 'ClipboardAndPrimarySelection'),
+      make_mouse_binding('Up', 1, 'Left', 'SHIFT', wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor 'ClipboardAndPrimarySelection'),
+      make_mouse_binding('Up', 1, 'Left', 'ALT', wezterm.action.CompleteSelection 'ClipboardAndPrimarySelection'),
+      make_mouse_binding('Up', 1, 'Left', 'SHIFT|ALT', wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor 'ClipboardAndPrimarySelection'),
+      make_mouse_binding('Up', 2, 'Left', 'NONE', wezterm.action.CompleteSelection 'ClipboardAndPrimarySelection'),
+      make_mouse_binding('Up', 3, 'Left', 'NONE', wezterm.action.CompleteSelection 'ClipboardAndPrimarySelection'),
     },
 
     pane_focus_follows_mouse = true,
