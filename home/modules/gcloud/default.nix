300920a136408a9aea1b57cf1a88e5cc87469096
Vincent Desjardins <vdesjardins@gmail.com>
2024-01-08 08:15:47 -0500

    fix(gcloud): refactor changes not applied

--- home/modules/gcloud/default.nix
+++ home/modules/gcloud/default.nix
@@ -4,27 +4,30 @@
   config,
   ...
 }:
-with lib;
-  mkMerge [
-    (
-      mkIf config.programs.zsh.enable {
-        home.packages = with pkgs.unstable; [
-          (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
-        ];
+with lib; let
+  cfg = config.programs.gcloud;
+in {
+  options.programs.gcloud = {
+    enable = mkEnableOption "gcloud";
+  };
 
-        home.sessionVariables = {
-          USE_GKE_GCLOUD_AUTH_PLUGIN = "True";
-        };
+  config = mkIf cfg.enable {
+    home.packages = with pkgs.unstable; [
+      (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
+    ];
 
-        programs.zsh = {
-          shellAliases = {
-            gpl = "gcloud projects list";
-          };
-        };
-        xdg.configFile."zsh/functions/gcp-project-id".source =
-          ./zsh/functions/gcp-project-id;
-        xdg.configFile."zsh/functions/gcp-list-ip".source =
-          ./zsh/functions/gcp-list-ip;
-      }
-    )
-  ]
+    home.sessionVariables = {
+      USE_GKE_GCLOUD_AUTH_PLUGIN = "True";
+    };
+
+    programs.zsh = {
+      shellAliases = {
+        gpl = "gcloud projects list";
+      };
+    };
+    xdg.configFile."zsh/functions/gcp-project-id".source =
+      ./zsh/functions/gcp-project-id;
+    xdg.configFile."zsh/functions/gcp-list-ip".source =
+      ./zsh/functions/gcp-list-ip;
+  };
+}
