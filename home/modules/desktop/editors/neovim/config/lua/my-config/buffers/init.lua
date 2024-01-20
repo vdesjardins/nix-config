5bf16bdedf76c77f304e8a6e3a1ae849f38eb9a7
Vincent Desjardins <vdesjardins@gmail.com>
2024-01-20 13:26:17 -0500

    feat(neovim): add delete/kill all buffers

--- home/modules/desktop/editors/neovim/config/lua/my-config/buffers/init.lua
+++ home/modules/desktop/editors/neovim/config/lua/my-config/buffers/init.lua
@@ -2,9 +2,11 @@ local wk = require("which-key")
 wk.register({
     b = {
         name = "buffer",
-        d = { "<cmd>Bdelete<cr>", "delete-buffer" },
+        d = { "<cmd>bdelete<cr>", "delete-buffer" },
+        D = { "<cmd>bufdo bdelete<cr>", "delete-buffer (all)" },
         f = { "<cmd>brewind<cr>", "first-buffer" },
-        k = { "<cmd>Bwipeout<cr>", "kill-buffer" },
+        k = { "<cmd>bwipeout<cr>", "kill-buffer" },
+        K = { "<cmd>bufdo bwipeout<cr>", "kill-buffer (all)" },
         l = { "<cmd>blast<cr>", "last-buffer" },
         n = { "<cmd>bnext<cr>", "next-buffer" },
         p = { "<cmd>bprev<cr>", "previous-buffer" },
