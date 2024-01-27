419aa3afe8913205146bf4a683d9e0bb4482b787
Vincent Desjardins <vdesjardins@gmail.com>
2024-01-26 20:28:03 -0500

    chore(systems): remove quotes

--- hosts/systems/linux.nix
+++ hosts/systems/linux.nix
@@ -1,6 +1,6 @@
 {lib, ...}: let
   inherit (lib.my) mkNixosSystem;
 in {
-  "dev-vm" = mkNixosSystem ../modules/linux/configurations/dev-vm.nix {system = "aarch64-linux";};
-  "home-server" = mkNixosSystem ../modules/linux/configurations/home-server.nix {system = "aarch64-linux";};
+  dev-vm = mkNixosSystem ../modules/linux/configurations/dev-vm.nix {system = "aarch64-linux";};
+  home-server = mkNixosSystem ../modules/linux/configurations/home-server.nix {system = "aarch64-linux";};
 }
