a71ce3994994753f76480a6b9e89a5d6aff3f2b5
Vincent Desjardins <vdesjardins@gmail.com>
2024-01-26 20:27:22 -0500

    chore(vm-builder): use new docker params

--- hosts/modules/linux/containers/vm-builder.nix
+++ hosts/modules/linux/containers/vm-builder.nix
@@ -11,26 +11,32 @@ in
     name = "nix-builder";
     tag = "latest";
     fromImage = nix.packages.${crossSystem}.dockerImage + "/image.tar.gz";
-    contents = with pkgsCross; [
-      qemu-utils
-      (pkgs.writeScriptBin
-        "create-vm"
-        ''
-          #!${pkgsCross.runtimeShell}
+    copyToRoot = with pkgsCross;
+      pkgs.buildEnv {
+        name = "image-root";
+        paths = [
+          qemu-utils
+          (pkgs.writeScriptBin
+            "create-vm"
+            ''
+              #!${pkgsCross.runtimeShell}
 
-          set -e
+              set -e
 
-          mkdir -p ~/.config/nix
-          echo 'experimental-features = nix-command flakes' >> ~/.config/nix/nix.conf
-          echo 'system-features = nixos-test benchmark big-parallel kvm' >> ~/.config/nix/nix.conf
-          echo 'substituters = https://cache.nixos.org/ https://vdesjardins.cachix.org' >> ~/.config/nix/nix.conf
-          echo 'trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= vdesjardins.cachix.org-1:o0VX1pROMi3RAULObu4+OOCIZFpcKAR01Oxef2CdUx4=' >> ~/.config/nix/nix.conf
+              mkdir -p ~/.config/nix
+              echo 'experimental-features = nix-command flakes' >> ~/.config/nix/nix.conf
+              echo 'system-features = nixos-test benchmark big-parallel kvm' >> ~/.config/nix/nix.conf
+              echo 'substituters = https://cache.nixos.org/ https://vdesjardins.cachix.org' >> ~/.config/nix/nix.conf
+              echo 'trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= vdesjardins.cachix.org-1:o0VX1pROMi3RAULObu4+OOCIZFpcKAR01Oxef2CdUx4=' >> ~/.config/nix/nix.conf
 
-          nix build "$@"
+              nix build "$@"
 
-          cp result/nixos-*-aarch64-linux.vmdk /output/
-        '')
-    ];
+              cp result/nixos-*-aarch64-linux.vmdk /output/
+            '')
+        ];
+        pathsToLink = ["/bin"];
+      };
+
     config = {
       # Cmd = [ "${pkgsCross.nix}/bin/nix" ];
       Cmd = ["/bin/create-vm"];
