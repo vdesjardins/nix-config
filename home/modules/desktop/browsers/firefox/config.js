20f226dc6b69b1695964216c79780b969ac788d2
Vincent Desjardins <vdesjardins@gmail.com>
2024-02-24 09:45:40 -0500

    docs(firefox): add references on key mappings

--- home/modules/desktop/browsers/firefox/config.js
+++ home/modules/desktop/browsers/firefox/config.js
@@ -1,4 +1,13 @@
-// https://superuser.com/questions/1271147/change-key-bindings-keyboard-shortcuts-in-firefox-57
+// Good references from github.com/leotaku/comprehensive-firefox
+//
+// [Surfingkeys](https://github.com/brookhong/Surfingkeys)
+// [default Firefox shortcuts](https://support.mozilla.org/en-US/kb/keyboard-shortcuts-perform-firefox-tasks-quickly)
+// [browser toolbox](https://firefox-source-docs.mozilla.org/devtools-user/browser_toolbox/index.html)
+// [remappable keybindings](https://searchfox.org/mozilla-release/source/browser/base/content/browser-sets.inc)
+// [this Stackoverflow question](https://superuser.com/questions/1271147/change-key-bindings-keyboard-shortcuts-in-firefox-quantum)
+// [official autoconf.js docs](https://support.mozilla.org/en-US/kb/customizing-firefox-using-autoconfig)
+// [reddit post](https://www.reddit.com/r/firefox/comments/yyxlw7/change_keyboard_shortcuts/)
+// [better prefs location?](https://bugzilla.mozilla.org/show_bug.cgi?id=1170092)
 try {
   let { classes: Cc, interfaces: Ci, manager: Cm  } = Components;
   const Services = globalThis.Services;
