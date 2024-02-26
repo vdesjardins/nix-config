69292a7d2e273e75e930b416b5e80e28767426b2
Vincent Desjardins <vdesjardins@gmail.com>
2024-02-26 07:17:46 -0500

    feat(nix): add new cmd tools

--- home/roles/dev/nix.nix
+++ home/roles/dev/nix.nix
@@ -4,9 +4,12 @@
   home.packages = with pkgs; [
     nixpkgs-fmt
     # nix-linter
+    nix-bisect # bisect nix builds
+    nix-init # generate nix packages
     nix-tree
     nix-prefetch
     nix-output-monitor
+    nurl # generate nix fetcher calls
     nvd # package version diff tool
   ];
 }
