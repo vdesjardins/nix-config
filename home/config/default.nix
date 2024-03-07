b869dc40422a3907a144db9d8a92bf6f5cbaf3c7
Vincent Desjardins <vdesjardins@gmail.com>
2024-03-06 22:00:33 -0500

    feat(hosts): add dedicated faclon user

--- home/config/default.nix
+++ home/config/default.nix
@@ -2,7 +2,7 @@
   inherit (lib.my) mkHomeConfiguration;
 in {
   "vince@dev-vm" = mkHomeConfiguration ../users/vince-vm.nix {system = "aarch64-linux";};
-  "vince@falcon" = mkHomeConfiguration ../users/vince-vm.nix {system = "x86_64-linux";};
+  "vince@falcon" = mkHomeConfiguration ../users/vince-falcon.nix {system = "x86_64-linux";};
   "vince@dev-mac" = mkHomeConfiguration ../users/vince-mac.nix {system = "aarch64-darwin";};
   "inf10906@V07P6L7R6H" = mkHomeConfiguration ../users/inf10906.nix {system = "aarch64-darwin";};
 }
---
+++ home/users/vince-falcon.nix
@@ -0,0 +1,48 @@
+{lib, ...}: {
+  home = {
+    username = "vince";
+    homeDirectory = "/home/vince";
+    stateVersion = "23.11";
+  };
+
+  imports = [
+    ../roles/common.nix
+
+    ../roles/win/darwin.nix
+    ../roles/dev/bash.nix
+    ../roles/dev/cue.nix
+    ../roles/dev/debugging.nix
+    ../roles/dev/go.nix
+    ../roles/dev/go-template.nix
+    ../roles/dev/js.nix
+    ../roles/dev/json.nix
+    ../roles/dev/jq.nix
+    ../roles/dev/lua.nix
+    ../roles/dev/make.nix
+    ../roles/dev/markdown.nix
+    ../roles/dev/nix.nix
+    ../roles/dev/python.nix
+    ../roles/dev/regex.nix
+    ../roles/dev/rust.nix
+    ../roles/dev/terraform.nix
+    ../roles/dev/vimscript.nix
+    ../roles/dev/yaml.nix
+    ../roles/dev/zig.nix
+    ../roles/ops/aws.nix
+    ../roles/ops/container.nix
+    ../roles/ops/k8s.nix
+    ../roles/ops/networking.nix
+    ../roles/ops/virtualization.nix
+    ../roles/security.nix
+    ../roles/utils.nix
+    ../roles/nixpkgs.nix
+    ../roles/sync.nix
+    ../roles/win/browsers.nix
+
+    ../roles/multimedia.nix
+    ../roles/win/viewers.nix
+    ../roles/win/security.nix
+    ../roles/win/sway.nix
+    ../roles/win/gaming.nix
+  ];
+}
