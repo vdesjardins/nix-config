f9beb44237b1fb3984ba58ab4cbc51199404e08b
Vincent Desjardins <vdesjardins@gmail.com>
2024-04-07 12:51:56 -0400

    feat(firefox): add nix options search

--- home/modules/desktop/browsers/firefox/default.nix
+++ home/modules/desktop/browsers/firefox/default.nix
@@ -147,6 +147,25 @@ in {
                 icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
                 definedAliases = ["@np"];
               };
+              "Nix Options" = {
+                urls = [
+                  {
+                    template = "https://search.nixos.org/options";
+                    params = [
+                      {
+                        name = "type";
+                        value = "packages";
+                      }
+                      {
+                        name = "query";
+                        value = "{searchTerms}";
+                      }
+                    ];
+                  }
+                ];
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = ["@no"];
+              };
               "NixOS Wiki" = {
                 urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
                 iconUpdateURL = "https://nixos.wiki/favicon.png";
