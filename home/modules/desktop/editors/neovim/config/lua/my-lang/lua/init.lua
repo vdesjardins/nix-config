26efceb3169780dad3fa227beaff57bfc1317067
Vincent Desjardins <vdesjardins@gmail.com>
2024-05-04 11:02:27 -0400

    feat(neovim): remove null-ls lua lint
    Signed-off-by: Vincent Desjardins <vdesjardins@gmail.com>

--- home/modules/desktop/editors/neovim/config/lua/my-lang/lua/init.lua
+++ home/modules/desktop/editors/neovim/config/lua/my-lang/lua/init.lua
@@ -23,4 +23,3 @@ require("lspconfig").lua_ls.setup({
 
 local null_ls = require("null-ls")
 _G.null_ls_sources[#_G.null_ls_sources + 1] = null_ls.builtins.formatting.stylua
-_G.null_ls_sources[#_G.null_ls_sources + 1] = null_ls.builtins.diagnostics.selene
