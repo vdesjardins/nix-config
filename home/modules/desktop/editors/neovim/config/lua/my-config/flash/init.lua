3dd352d583d1d240787dc97d6d4b83376105c1ac
Vincent Desjardins <vdesjardins@gmail.com>
2024-05-18 12:35:15 -0400

    feat(neovim): configure flash

--- home/modules/desktop/editors/neovim/config/lua/my-config/flash/init.lua
+++ home/modules/desktop/editors/neovim/config/lua/my-config/flash/init.lua
@@ -1,1 +1,62 @@
-require("flash").setup({})
+require("flash").setup({
+    modes = {
+        search = {
+            enabled = true,
+        },
+    },
+})
+
+-- keybindings
+local wk = require("which-key")
+
+for _, m in ipairs({ [1] = "n", [2] = "x", [3] = "o" }) do
+    wk.register({
+        s = {
+            function()
+                require("flash").jump()
+            end,
+            "Flash",
+        },
+        S = {
+            function()
+                require("flash").treesitter()
+            end,
+            "Flash Treesitter",
+        },
+    }, { mode = m })
+end
+
+wk.register({
+    r = {
+        function()
+            require("flash").remote()
+        end,
+        "Remote Flash",
+    },
+}, { mode = "o" })
+
+wk.register({
+    R = {
+        function()
+            require("flash").treesitter_search()
+        end,
+        "Treesitter Search",
+    },
+}, { mode = "o" })
+wk.register({
+    R = {
+        function()
+            require("flash").treesitter_search()
+        end,
+        "Treesitter Search",
+    },
+}, { mode = "x" })
+
+wk.register({
+    ["<c-s>"] = {
+        function()
+            require("flash").toggle()
+        end,
+        "Toggle Flash Search",
+    },
+}, { mode = "c" })
