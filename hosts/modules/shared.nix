aeacb02a4636e63b001bca5c05cdd4f4903650a9
Vincent Desjardins <vdesjardins@gmail.com>
2024-05-25 17:08:55 -0400

    feat(nix): add nix-community cache substituter
    Signed-off-by: Vincent Desjardins <vdesjardins@gmail.com>

--- hosts/modules/shared.nix
+++ hosts/modules/shared.nix
@@ -15,12 +15,14 @@ in {
         "https://cache.nixos.org/"
         "https://vdesjardins.cachix.org"
         "https://cache.ngi0.nixos.org/" # ca-derivations cache
+        "https://nix-community.cachix.org"
       ];
 
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "vdesjardins.cachix.org-1:o0VX1pROMi3RAULObu4+OOCIZFpcKAR01Oxef2CdUx4="
         "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
 
       keep-going = true;
