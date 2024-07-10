c0c7674d72bfa830b91a24248b614673851bebfc
Vincent Desjardins <vdesjardins@gmail.com>
2024-07-10 06:57:11 -0400

    feat(debugging): gdb only compiles on linux
    Signed-off-by: Vincent Desjardins <vdesjardins@gmail.com>

--- home/roles/dev/debugging.nix
+++ home/roles/dev/debugging.nix
@@ -2,14 +2,14 @@
   home.packages = with pkgs;
     [
       binutils
-      gdb
-      gdbgui
       lldb
       mitmproxy
       mitmproxy2swagger
       xxd
     ]
     ++ lib.optionals stdenv.isLinux [
+      gdb
+      gdbgui
       cgdb
       uftrace # Function graph tracer for C/C++/Rust
       rr-unstable
