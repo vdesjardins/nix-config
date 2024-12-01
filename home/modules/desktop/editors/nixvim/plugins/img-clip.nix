0cb5ff548d9425ba6152e5d01c95c4871d7a7a2f
Vincent Desjardins <vdesjardins@gmail.com>
2024-12-01 18:46:23 -0500

    feat(neovim): set some img config related to avante

--- home/modules/desktop/editors/nixvim/plugins/img-clip.nix
+++ home/modules/desktop/editors/nixvim/plugins/img-clip.nix
@@ -11,7 +11,15 @@
     extraConfigLua =
       # lua
       ''
-        require("img-clip").setup({})
+        require("img-clip").setup({
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+        },
+        })
       '';
   };
 
