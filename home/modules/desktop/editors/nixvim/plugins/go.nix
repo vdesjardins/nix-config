96c518bb5d82d8b5e926519d020442a5afee0202
Vincent Desjardins <vdesjardins@gmail.com>
2024-12-11 19:41:13 -0500

    fix(neovim): golang config

--- home/modules/desktop/editors/nixvim/plugins/go.nix
+++ home/modules/desktop/editors/nixvim/plugins/go.nix
@@ -4,19 +4,21 @@
       enable = true;
 
       settings = {
-        analyses = {
-          unusedparams = true;
-        };
-        staticcheck = true;
-        gofumpt = true;
-        hints = {
-          assignVariableTypes = true;
-          compositeLiteralFields = true;
-          compositeLiteralTypes = true;
-          constantValues = true;
-          functionTypeParameters = true;
-          parameterNames = true;
-          rangeVariableTypes = true;
+        gopls = {
+          analyses = {
+            unusedparams = true;
+          };
+          staticcheck = true;
+          gofumpt = true;
+          hints = {
+            assignVariableTypes = true;
+            compositeLiteralFields = true;
+            compositeLiteralTypes = true;
+            constantValues = true;
+            functionTypeParameters = true;
+            parameterNames = true;
+            rangeVariableTypes = true;
+          };
         };
       };
     };
