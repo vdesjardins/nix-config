40bc4e204990d0f32da25e0cac34f67c458e5def
Vincent Desjardins <vdesjardins@gmail.com>
2024-12-13 20:19:33 -0500

    feat(neovim): disable mini.files netwrw and add shortcuts

--- home/modules/desktop/editors/nixvim/plugins/mini.nix
+++ home/modules/desktop/editors/nixvim/plugins/mini.nix
@@ -1,21 +1,50 @@
 {
-  programs.nixvim.plugins.mini = {
-    enable = true;
+  programs.nixvim = {
+    plugins.mini = {
+      enable = true;
 
-    modules = {
-      icons = {};
-      files = {};
-      surround = {
-        mappings = {
-          add = "gsa";
-          delete = "gsd";
-          find = "gsf";
-          find_left = "gsF";
-          highlight = "gsh";
-          replace = "gsr";
-          update_n_lines = "gsn";
+      modules = {
+        icons = {};
+        files = {
+          options = {
+            use_as_default_explorer = false;
+          };
+        };
+        surround = {
+          mappings = {
+            add = "gsa";
+            delete = "gsd";
+            find = "gsf";
+            find_left = "gsF";
+            highlight = "gsh";
+            replace = "gsr";
+            update_n_lines = "gsn";
+          };
         };
       };
     };
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>bP";
+        action.__raw = ''
+          function()
+            MiniFiles.open()
+          end
+        '';
+        options.desc = "Browse project's files (Mini)";
+      }
+      {
+        mode = "n";
+        key = "<leader>bB";
+        action.__raw = ''
+          function()
+            MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+          end
+        '';
+        options.desc = "Browse buffer's files (Mini)";
+      }
+    ];
   };
 }
