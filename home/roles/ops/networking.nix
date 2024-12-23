55545e5bccedcc9d8c63937a3f3935160eb27b46
Vincent Desjardins <vdesjardins@gmail.com>
2024-12-23 09:34:01 -0500

    feat(perf): install perf

--- home/roles/ops/networking.nix
+++ home/roles/ops/networking.nix
@@ -1,8 +1,9 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     dig
-    dogdns
     dnspeep # look at host dns queries
+    dogdns
+    iperf
     mtr # network diagnostic tool
     nmap
     tcpdump
--- hosts/modules/linux/shared/debugging.nix
+++ hosts/modules/linux/shared/debugging.nix
@@ -1,3 +1,10 @@
-{...}: {
+{config, ...}: {
   services.nixseparatedebuginfod.enable = true;
+
+  environment.systemPackages = [config.boot.kernelPackages.perf];
+  boot.kernel.sysctl = {
+    "kernel.ftrace_enabled" = true;
+    "kernel.perf_event_paranoid" = 1;
+    "kernel.kernel.kptr_restrict" = 1;
+  };
 }
