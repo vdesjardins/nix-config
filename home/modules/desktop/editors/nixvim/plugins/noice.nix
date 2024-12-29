eff3ec7bc66330c2592207b959a9a3c75344169e
Vincent Desjardins <vdesjardins@gmail.com>
2024-12-28 21:21:48 -0500

    feat(neovim): cmdline at the bottom and more standard options

--- home/modules/desktop/editors/nixvim/plugins/noice.nix
+++ home/modules/desktop/editors/nixvim/plugins/noice.nix
@@ -3,7 +3,7 @@
     plugins.noice = {
       enable = true;
       settings = {
-        notify.enabled = true;
+        cmdline.view = "cmdline";
 
         lsp = {
           # override markdown rendering so that **cmp** and other plugins use **Treesitter**
@@ -16,10 +16,10 @@
         # you can enable a preset for easier configuration
         presets = {
           bottom_search = true; # use a classic bottom cmdline for search
-          command_palette = true; # position the cmdline and popupmenu together
-          long_message_to_split = true; # long messages will be sent to a split
+          command_palette = false; # position the cmdline and popupmenu together
+          long_message_to_split = false; # long messages will be sent to a split
           inc_rename = false; # enables an input dialog for inc-rename.nvim
-          lsp_doc_border = true; # add a border to hover docs and signature help
+          lsp_doc_border = false; # add a border to hover docs and signature help
         };
       };
     };
