37a9463e23d94bea2baa5ab710842097a03852e5
Vincent Desjardins <vdesjardins@gmail.com>
2025-01-04 11:57:49 -0500

    feat(neovim): define dap icons

--- home/modules/desktop/editors/nixvim/plugins/dap.nix
+++ home/modules/desktop/editors/nixvim/plugins/dap.nix
@@ -9,6 +9,14 @@
       };
     };
 
+    extraConfigLua = ''
+      vim.fn.sign_define("DapBreakpoint", { text = "● ", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
+      vim.fn.sign_define("DapBreakpointCondition", { text = "● ", texthl = "DiagnosticSignWarn", linehl = "", numhl = "" })
+      vim.fn.sign_define("DapLogPoint", { text = "● ", texthl = "DiagnosticSignInfo", linehl = "", numhl = "" })
+      vim.fn.sign_define("DapStopped", { text = "→ ", texthl = "DiagnosticSignWarn", linehl = "", numhl = "" })
+      vim.fn.sign_define("DapBreakpointReject", { text = "●", texthl = "DiagnosticSignHint", linehl = "", numhl = "" })
+    '';
+
     keymaps = [
       {
         mode = "n";
