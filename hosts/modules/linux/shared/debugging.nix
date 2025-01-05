a0b7f9d2972b37de29f721d126fd85bf06527aba
Vincent Desjardins <vdesjardins@gmail.com>
2025-01-05 14:39:06 -0500

    feat(hosts): permit perf without root

--- hosts/modules/linux/shared/debugging.nix
+++ hosts/modules/linux/shared/debugging.nix
@@ -5,6 +5,6 @@
   boot.kernel.sysctl = {
     "kernel.ftrace_enabled" = true;
     "kernel.perf_event_paranoid" = 1;
-    "kernel.kernel.kptr_restrict" = 1;
+    "kernel.kernel.kptr_restrict" = 0;
   };
 }
