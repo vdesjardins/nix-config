811733a3866c2ee6de589952541f345f6c83d2f6
Vincent Desjardins <vdesjardins@gmail.com>
2025-01-21 08:20:46 -0500

    chore(neovim): update snacks

--- overlays/nixpkgs/snacks-nvim.nix
+++ overlays/nixpkgs/snacks-nvim.nix
@@ -4,13 +4,13 @@ _inputs: _final: prev: {
     // {
       snacks-nvim = prev.vimUtils.buildVimPlugin {
         pname = "snacks.nvim";
-        version = "2025-01-19";
+        version = "2.14.0";
 
         src = prev.fetchFromGitHub {
           owner = "folke";
           repo = "snacks.nvim";
-          rev = "eb0e5b7efe603bea7a0823ffaed13c52b395d04b";
-          hash = "sha256-T015Pt9HfzABHQSrH40Gi798T/oTJD1mtc2/a63IYhw=";
+          rev = "v2.14.0";
+          hash = "sha256-BpTN7tkIHo07Mb3g07OQDfNdfLeh48f8NNPXYM8eQCk=";
         };
 
         doCheck = false;
