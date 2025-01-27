3d8d7b41b0c24495e68184874329146d2f6d725c
Vincent Desjardins <vdesjardins@gmail.com>
2025-01-27 07:15:18 -0500

    chore(neovim): bump codecompanion

--- overlays/nixpkgs/codecompanion.nix
+++ overlays/nixpkgs/codecompanion.nix
@@ -4,13 +4,13 @@ _inputs: _final: prev: {
     // {
       codecompanion = prev.vimUtils.buildVimPlugin rec {
         pname = "codecompanion.nvim";
-        version = "11.15.0";
+        version = "11.15.3";
 
         src = prev.fetchFromGitHub {
           owner = "olimorris";
           repo = "codecompanion.nvim";
           rev = "v${version}";
-          hash = "sha256-ab0PkaBSgwd8AeKGSdyZuod1sI+4Y+FTB+KeT3B7XHM=";
+          hash = "sha256-QKrRHe1NrmfRPk2hltU8ckRJYwqNH/pieiR93vMlmsQ=";
         };
 
         doCheck = false;
