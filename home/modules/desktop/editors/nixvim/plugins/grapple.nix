74c938e337d58639a94ce39169d09ac42c433522
Vincent Desjardins <vdesjardins@gmail.com>
2025-02-22 16:49:20 -0500

    feat(neovim): better goto

--- home/modules/desktop/editors/nixvim/plugins/grapple.nix
+++ home/modules/desktop/editors/nixvim/plugins/grapple.nix
@@ -1,4 +1,11 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (lib.lists) genList;
+  inherit (builtins) toString;
+in {
   programs.nixvim = {
     extraPlugins = with pkgs.vimPlugins; [
       grapple-nvim
@@ -8,31 +15,47 @@
       require("grapple").setup({})
     '';
 
-    keymaps = [
-      {
+    keymaps =
+      [
+        {
+          mode = "n";
+          key = "<leader>gm";
+          action = "<cmd>Grapple tag<cr>";
+          options.desc = "Toggle Tag (Grapple)";
+        }
+        {
+          mode = "n";
+          key = "<leader>g/";
+          action = "<cmd>Grapple toggle_tags<cr>";
+          options.desc = "Open Tags Window (Grapple)";
+        }
+        {
+          mode = "n";
+          key = "g/";
+          action = "<cmd>Grapple toggle_tags<cr>";
+          options.desc = "Open Tags Window (Grapple)";
+        }
+        {
+          mode = "n";
+          key = "<leader>gn";
+          action = "<cmd>Grapple cycle_tags next<cr>";
+          options.desc = "Cycle Tags Next (Grapple)";
+        }
+        {
+          mode = "n";
+          key = "<leader>gp";
+          action = "<cmd>Grapple cycle_tags prev<cr>";
+          options.desc = "Cycle Tags Previous (Grapple)";
+        }
+      ]
+      ++ genList (i: let
+        idx = toString (i + 1);
+      in {
         mode = "n";
-        key = "<leader>gm";
-        action = "<cmd>Grapple toggle<cr>";
-        options.desc = "Toggle Tag (Grapple)";
-      }
-      {
-        mode = "n";
-        key = "<leader>gM";
-        action = "<cmd>Grapple toggle_tags<cr>";
-        options.desc = "Open Tags Window (Grapple)";
-      }
-      {
-        mode = "n";
-        key = "<leader>gn";
-        action = "<cmd>Grapple cycle_next<cr>";
-        options.desc = "Cycle Next (Grapple)";
-      }
-      {
-        mode = "n";
-        key = "<leader>gp";
-        action = "<cmd>Grapple cycle_prev<cr>";
-        options.desc = "Cycle Previous (Grapple)";
-      }
-    ];
+        key = "g${idx}";
+        action = "<cmd>Grapple select index=${idx}<cr>";
+        options.desc = "Goto Tag ${idx} (Grapple)";
+      })
+      9;
   };
 }
