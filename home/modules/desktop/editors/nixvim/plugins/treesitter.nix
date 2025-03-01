9536e5ee086d28b0a048781e7c5981018fb77fac
Vincent Desjardins <vdesjardins@gmail.com>
2025-02-28 19:42:17 -0500

    feat(neovim): enable treesitterincremental selection

--- home/modules/desktop/editors/nixvim/plugins/treesitter.nix
+++ home/modules/desktop/editors/nixvim/plugins/treesitter.nix
@@ -9,6 +9,15 @@
         settings = {
           highlight.enable = true;
           indent.enable = true;
+          incremental_selection = {
+            enable = true;
+            keymaps = {
+              init_selection = false;
+              node_incremental = "v";
+              node_decremental = "V";
+              scope_incremental = "grc";
+            };
+          };
         };
       };
 
