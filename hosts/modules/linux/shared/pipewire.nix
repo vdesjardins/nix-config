c9c9e66c0e1044dc94dff96bab78016c4ed8303e
Vincent Desjardins <vdesjardins@gmail.com>
2025-03-17 17:10:59 -0400

    feat(sound): prevent audio sinks sleep

--- hosts/modules/linux/shared/pipewire.nix
+++ hosts/modules/linux/shared/pipewire.nix
@@ -1,12 +1,59 @@
-{...}: {
+{pkgs, ...}: {
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+
     alsa.enable = true;
     alsa.support32Bit = true;
+
     pulse.enable = true;
-    wireplumber.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+
+    wireplumber = {
+      enable = true;
+
+      extraConfig = {
+        "90-disable-suspension" = {
+          "monitor.alsa.rules" = [
+            {
+              matches = [
+                {
+                  # Matches all sources
+                  "node.name" = "~alsa_input.*";
+                }
+                {
+                  # Matches all sinks
+                  "node.name" = "~alsa_output.*";
+                }
+              ];
+              actions = {
+                update-props = {
+                  "session.suspend-timeout-seconds" = 0;
+                };
+              };
+            }
+          ];
+
+          "monitor.bluez.rules" = [
+            {
+              matches = [
+                {
+                  # Matches all sources
+                  "node.name" = "~bluez_input.*";
+                }
+                {
+                  # Matches all sinks
+                  "node.name" = "~bluez_output.*";
+                }
+              ];
+              actions = {
+                update-props = {
+                  "session.suspend-timeout-seconds" = 0;
+                };
+              };
+            }
+          ];
+        };
+      };
+    };
   };
 }
