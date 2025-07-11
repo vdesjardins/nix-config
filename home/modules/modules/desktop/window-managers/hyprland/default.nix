a5a5b170430d78c523a10487b74774a2e7f66365
Vincent Desjardins <vdesjardins@gmail.com>
2025-07-11 07:05:09 -0400

    feat(hyprland): enable xwayland

--- home/modules/modules/desktop/window-managers/hyprland/default.nix
+++ home/modules/modules/desktop/window-managers/hyprland/default.nix
@@ -29,16 +29,16 @@
   '';
 
   monitor-attached = pkgs.writeShellScript "monitor-attached" ''
-    hyprctl dispatch moveworkspacetomonitor 10 0
-    hyprctl dispatch moveworkspacetomonitor 9 0
-    hyprctl dispatch moveworkspacetomonitor 8 0
-    hyprctl dispatch moveworkspacetomonitor 7 0
-    hyprctl dispatch moveworkspacetomonitor 6 1
-    hyprctl dispatch moveworkspacetomonitor 5 1
-    hyprctl dispatch moveworkspacetomonitor 4 1
-    hyprctl dispatch moveworkspacetomonitor 3 1
-    hyprctl dispatch moveworkspacetomonitor 2 1
     hyprctl dispatch moveworkspacetomonitor 1 1
+    hyprctl dispatch moveworkspacetomonitor 2 1
+    hyprctl dispatch moveworkspacetomonitor 3 1
+    hyprctl dispatch moveworkspacetomonitor 4 1
+    hyprctl dispatch moveworkspacetomonitor 5 1
+    hyprctl dispatch moveworkspacetomonitor 6 0
+    hyprctl dispatch moveworkspacetomonitor 7 0
+    hyprctl dispatch moveworkspacetomonitor 8 0
+    hyprctl dispatch moveworkspacetomonitor 9 0
+    hyprctl dispatch moveworkspacetomonitor 10 0
   '';
 in {
   options.modules.desktop.window-managers.hyprland = {
@@ -177,6 +177,8 @@ in {
 
     wayland.windowManager.hyprland = {
       enable = true;
+
+      xwayland.enable = true;
 
       settings = {
         "$terminal" = "ghostty";
