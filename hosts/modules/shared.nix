4e5ccbdaa81071c25d5f511a8b48bbeb0a33481b
Vincent Desjardins <vdesjardins@gmail.com>
2025-12-20 11:45:25 -0500

    fix(hosts): nix conf restore

--- hosts/modules/shared.nix
+++ hosts/modules/shared.nix
@@ -1,41 +1,41 @@
 # This file contains configuration that is shared across all hosts.
 {pkgs, ...}: {
-  # nix = {
-  #   optimise.automatic = true;
-  #
-  #   settings = {
-  #     substituters = [
-  #       "https://cache.nixos.org/"
-  #       "https://nix-community.cachix.org"
-  #     ];
-  #
-  #     trusted-public-keys = [
-  #       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-  #       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-  #     ];
-  #
-  #     keep-going = true;
-  #     experimental-features = ["nix-command" "flakes" "ca-derivations"];
-  #     keep-derivations = true;
-  #     keep-outputs = true;
-  #
-  #     trusted-users = ["@wheel" "root"];
-  #
-  #     fallback = true;
-  #     warn-dirty = false;
-  #   };
-  #
-  #   # gc = {
-  #   #   automatic = true;
-  #   #   options = "--delete-older-than +5";
-  #   # };
-  #
-  #   package = pkgs.nixVersions.nix_2_28;
-  # };
-  #
-  # environment.systemPackages = with pkgs; [
-  #   nixVersions.nix_2_28
-  # ];
+  nix = {
+    optimise.automatic = true;
+
+    settings = {
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+
+      keep-going = true;
+      experimental-features = ["nix-command" "flakes" "ca-derivations"];
+      keep-derivations = true;
+      keep-outputs = true;
+
+      trusted-users = ["@wheel" "root"];
+
+      fallback = true;
+      warn-dirty = false;
+    };
+
+    gc = {
+      automatic = true;
+      options = "--delete-older-than +5";
+    };
+
+    package = pkgs.nixVersions.nix_2_28;
+  };
+
+  environment.systemPackages = with pkgs; [
+    nixVersions.nix_2_28
+  ];
 
   programs = {
     zsh = {
