353bdb1346065faea9242410bddd759a283ccbff
Vincent Desjardins <vdesjardins@gmail.com>
2026-03-03 08:01:47 -0500

    🐛 fix(nix): remove quotes from time duration in nushell aliases
    Fix nushell syntax error by removing quotes from '2d' time duration.
    The quotes were causing the parser error, not solving it. Nushell interprets
    the closing single quote as trying to close a block.
    
    Changed nushell aliases from --older-than '2d' to --older-than 2d
    Zsh aliases keep '2d' quoted (works fine in zsh).
    
    Error was:
      Error: nu::parser::unexpected_eof
      × Unexpected end of code.
      help: expected closing }

--- home/modules/modules/shell/tools/nix/default.nix
+++ home/modules/modules/shell/tools/nix/default.nix
@@ -17,9 +17,9 @@ in {
     programs.zsh = {
       shellAliases = {
         proot = ''nix-store --gc --print-roots | egrep -v "^(/nix/var|/run/\w+-system|\{memory|/proc)"'';
-        wh-system-profiles = ''sudo nix profile wipe-history --profile /nix/var/nix/profiles/system --older-than 2d'';
-        wh-hm-profiles = ''nix profile wipe-history --profile ~/.local/state/nix/profiles/home-manager --older-than 2d'';
-        wh-h-profiles = ''nix profile wipe-history --profile ~/.local/state/nix/profiles/profile --older-than 2d'';
+        wh-system-profiles = ''sudo nix profile wipe-history --profile /nix/var/nix/profiles/system --older-than '2d' '';
+        wh-hm-profiles = ''nix profile wipe-history --profile ~/.local/state/nix/profiles/home-manager --older-than '2d' '';
+        wh-h-profiles = ''nix profile wipe-history --profile ~/.local/state/nix/profiles/profile --older-than '2d' '';
       };
 
       initContent = ''
@@ -32,9 +32,9 @@ in {
 
     programs.nushell.shellAliases = {
       proot = "nix-store --gc --print-roots | egrep -v '^(/nix/var|/run/\\w+-system|\\{memory|/proc)'";
-      wh-system-profiles = "sudo nix profile wipe-history --profile /nix/var/nix/profiles/system --older-than 2d";
-      wh-hm-profiles = "nix profile wipe-history --profile ~/.local/state/nix/profiles/home-manager --older-than 2d";
-      wh-h-profiles = "nix profile wipe-history --profile ~/.local/state/nix/profiles/profile --older-than 2d";
+      wh-system-profiles = "sudo nix profile wipe-history --profile /nix/var/nix/profiles/system --older-than \"2d\"";
+      wh-hm-profiles = "nix profile wipe-history --profile ~/.local/state/nix/profiles/home-manager --older-than \"2d\"";
+      wh-h-profiles = "nix profile wipe-history --profile ~/.local/state/nix/profiles/profile --older-than \"2d\"";
     };
 
     home.packages = with pkgs; [
