65915880b31d8bd49bca59ce351ad454121d1b99
Vincent Desjardins <vdesjardins@gmail.com>
2026-07-25 23:11:06 -0400

    chore(deps): update ghcr.io/home-assistant/home-assistant docker tag to v2026.7.4 (#168)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- hosts/modules/linux/shared/home-assistant.nix
+++ hosts/modules/linux/shared/home-assistant.nix
@@ -87,7 +87,7 @@ in {
     containers = {
       home-assistant = {
         # renovate: datasource=docker depName=home-assistant/home-assistant
-        image = "ghcr.io/home-assistant/home-assistant:2026.7.2";
+        image = "ghcr.io/home-assistant/home-assistant:2026.7.4";
 
         environment.TZ = config.time.timeZone;
 
